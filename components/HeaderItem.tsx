05f5ea5042aac4f5c82a0f7590ba2645ac98a529
ETAnderson <anderson.eric.t@gmail.com>
2022-01-15 11:39:34 -0500

    finally got state WAI

--- components/HeaderItem.tsx
+++ components/HeaderItem.tsx
@@ -7,7 +7,7 @@ type HeaderProps = {
 function HeaderItem({title, Icon, onClick }: HeaderProps ) {
  
     return (
-        <div onClick={() => onClick}
+        <div onClick={onClick}
             className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white">
             <Icon className='h-8 mb-1 group-hover:animate-bounce '/>
             <h1 className='opacity-0 group-hover:opacity-100 tracking-widest'>{title}</h1>
