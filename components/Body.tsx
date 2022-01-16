e53f49612c79192e13abe45e717f7fc17f46fbcf
ETAnderson <anderson.eric.t@gmail.com>
2022-01-16 16:42:01 -0500

    placeholders and basics in place: staging for initial deploy on vercel

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,3 +1,5 @@
+import Image from 'next';
+
 type BodyProps = {
     display: any,
 }
@@ -6,7 +8,7 @@ function Body({ display }: BodyProps) {
     return (
         <div className="flex flex-col items-center justify-center">
             <img
-                className="rounded-full mb-10" 
+                className="rounded-full w-1/3 h-1/3 mb-10 object-contain" 
                 src={display.image} alt="" />
            <p className="text-white mx-10">{display.text}</p>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/outline'
 
 
+
 type BodyProps = {
   image: string,
   text: string,
@@ -16,12 +17,12 @@ type BodyProps = {
 
 const Intro= {
   image: 'https://avatars.githubusercontent.com/u/2357717?v=4',
-  text: 'Hello this is gonna be the new INTRO body! Hopefully using the body as a component will make single page transitions cleaner!'
+  text: 'Hello this is placeholder text for the new INTRO body! The skills button and info button are working and more is coming so keep an eye out! Please feel free to contact me about a similiar site or web project, as I also develop web applications! anderson.eric.t@gmail.com'
 }
 
 const Skills = {
-  image: 'https://avatars.githubusercontent.com/u/2357717?v=4',
-  text: 'Hello this is gonna be the new SKILL body! Hopefully using the body as a component will make single page transitions cleaner!'
+  image:"/RainbowTree.jpg",
+  text: 'So far, this website has been built using the fantastic Nextjs, and in particular the React hook "useState" to manage a state and replicated multi-page functionality from a single page app, which provides a more pleasant user experience and better search engine optimization. The beautiful painting above, however, was made entirely by my even more beautiful wife, Rochelle!'
 }
 
 
