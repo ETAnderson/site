52641a352b399afa42701f6d7401c59968dc1a1f
ETAnderson <anderson.eric.t@gmail.com>
2022-01-18 18:57:09 -0500

    change github placeholder HeaderItem into its own external link

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,12 @@ export default function Home({ image, text }: BodyProps) {
 
           <HeaderItem onClick={() => onClick(Skills)} Icon={BeakerIcon} title='Skills' />
 
-          <HeaderItem  Icon={CollectionIcon} title='Github' />
+          <a className='group flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white' href='https://github.com/ETAnderson'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 group-hover:animate-bounce" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
+            </svg> 
+            <h1 className='opacity-0 group-hover:opacity-100 tracking-widest'> Github </h1>
+          </a>
 
         </div>
       </header> 
