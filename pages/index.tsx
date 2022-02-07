b630543b30c7110a728b25d71265e923d585ce10
ETAnderson <anderson.eric.t@gmail.com>
2022-02-07 13:43:23 -0500

    added downloadable resume to header

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import {
   CollectionIcon,
   BeakerIcon,
   MoonIcon,
+  DocumentDownloadIcon,
 } from "@heroicons/react/outline";
 import Dropdown from "../components/Dropdown";
 import { Display, Site } from "../typings";
@@ -91,6 +92,17 @@ export default function Home(
           <button onClick={() => setIsDropped(!isDropped)}>
             <Dropdown />
           </button>
+
+          <a
+            className="group flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white"
+            href="/Eric Anderson Resume.pdf"
+            download
+          >
+            <DocumentDownloadIcon className="h-8 mb-1 group-hover:animate-bounce" />
+            <h1 className="opacity-0 group-hover:opacity-100 tracking-widest">
+              Resume
+            </h1>
+          </a>
 
           <a
             className="group flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white"
